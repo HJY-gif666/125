fff17deadfe7b072fff5ac55680eefde5a94808d
HJY-gif666 <1197025392@qq.com>
2025-07-07 15:25:08 +0800

    Add files via upload

---
+++ model12.py
@@ -0,0 +1,33 @@
+import streamlit as st
+import pickle
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import LabelEncoder
+from PIL import Image  # 添加Image模块
+
+# Page configuration
+st.set_page_config(
+    page_title='Evaluating and forecasting undergraduate dropouts ',
+    layout='wide',
+    initial_sidebar_state='expanded'
+)
+
+# Title of the app centered with HTML and CSS
+st.markdown("""
+    <style>
+    .center-content {
+        text-align: center;
+    }
+    </style>
+    <h1 style='color: darkblue;' class='center-content'>Evaluating and forecasting undergraduate dropouts</h1>
+    """, unsafe_allow_html=True)
+
+# Additional info centered
+st.markdown("""
+    <div class='center-content'>
+    Developed by Dr. Songbo Wang et al., Hubei University of Technology .<br>
+    Email: Wangsongbo@hbut.edu.cn; 
+    </div>
+    """, unsafe_allow_html=True)
+
+st.write(' ')
