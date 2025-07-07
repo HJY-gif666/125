df27b7d90ff64ea3692d1f4cb97c2e310b2c6659
HJY-gif666 <1197025392@qq.com>
2025-07-07 15:21:40 +0800

    Add files via upload

--- model11.py
+++ model11.py
@@ -3,6 +3,7 @@ import pickle
 import pandas as pd
 import numpy as np
 from sklearn.preprocessing import LabelEncoder
+from PIL import Image  # 添加Image模块
 
 # Page configuration
 st.set_page_config(
@@ -30,14 +31,3 @@ st.markdown("""
     """, unsafe_allow_html=True)
 
 st.write(' ')
-# 显示封面图片
-image_path = '/mnt/data/图片1.tif'
-st.image(image_path, caption="欢迎使用模型预测", use_column_width=True)
-image = Image.open("Domestic and International.jpg")
-
-# 加载预训练模型
-try:
-    model1 = pickle.load(open('best_model_non1.pkl', 'rb'))  # 确保模型文件路径正确
-    model2 = pickle.load(open('best_model_int.pkl', 'rb'))
-except Exception as e:
-    st.error(f"加载模型时出错: {e}")
