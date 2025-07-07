084d5f22ef58eb8c862118981f676408988aae6b
HJY-gif666 <1197025392@qq.com>
2025-07-07 15:13:55 +0800

    Add files via upload

--- model11.py
+++ model11.py
@@ -4,9 +4,37 @@ import pandas as pd
 import numpy as np
 from sklearn.preprocessing import LabelEncoder
 
+# Page configuration
+st.set_page_config(
+    page_title='Evaluating and forecasting undergraduate dropouts ',
+    layout='wide',
+    initial_sidebar_state='expanded'
+)
+
+# Title of the app centered with HTML and CSS
+st.markdown("""
+    <style>
+    .center-content {
+        text-align: center;
+    }
+    </style>
+    <h1 style='color: darkblue;' class='center-content'>Evaluating and forecasting undergraduate dropouts</h1>
+    """, unsafe_allow_html=True)
+
+# Additional info centered
+st.markdown("""
+    <div class='center-content'>
+    Developed by Dr. Songbo Wang et al., Hubei University of Technology .<br>
+    Email: Wangsongbo@hbut.edu.cn; 
+    </div>
+    """, unsafe_allow_html=True)
+
+st.write(' ')
 # 显示封面图片
 image_path = '/mnt/data/图片1.tif'
 st.image(image_path, caption="欢迎使用模型预测", use_column_width=True)
+image = Image.open("Domestic and International.tif")
+resized_image = image.resize((430, 200))
 
 # 加载预训练模型
 try:
